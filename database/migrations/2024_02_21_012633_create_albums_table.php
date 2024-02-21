38ace930d37a479779cadf84ac5913ee5ed9ef3d
AngelDabnee <a22311046@uthermosillo.edu.mx>
2024-02-20 18:39:47 -0700

    feature(created):models and controllers about the db
    Signed-off-by: AngelDabnee <a22311046@uthermosillo.edu.mx>

---
+++ database/migrations/2024_02_21_012633_create_albums_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->string('user_id');
+            $table->string('date');
+            $table->string('produced');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('albums');
+    }
+};
---
+++ database/migrations/2024_02_21_013510_update_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users',function($table){
+            $table->string('bio');
+            $table->string('role_id');
+            $table->string('image')->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn('bio');
+            $table->dropColumn('role_id');
+            $table->dropColumn('image');
+        });
+    }
+};
