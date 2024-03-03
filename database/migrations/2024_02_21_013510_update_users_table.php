e95108cf573588be01d590362006a00d0e475596
JEstebanSanti <a20311083@uthermosillo.edu.mx>
2024-03-03 15:56:34 -0700

    feature(email-test): añade funcionalidad para la auth por email en desarrollo

--- database/migrations/2024_02_21_013510_update_users_table.php
+++ database/migrations/2024_02_21_013510_update_users_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users',function($table){
-            $table->string('bio')->after('password');
-            $table->string('role_id')->after('bio');
-            $table->string('image')->after('name');
+            $table->string('bio')->after('password')->nullable();
+            $table->string('role_id')->after('bio')->nullable();
+            $table->string('image')->after('name')->nullable();
         });
     }
 
