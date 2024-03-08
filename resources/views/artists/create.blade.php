f44517f449eea9f4c68cd30b7e78d03e95070731
AngelDabnee <a22311046@uthermosillo.edu.mx>
2024-03-07 22:31:15 -0700

    feature()configuracion pendiente para terminar el create y a su vez el update
    Signed-off-by: AngelDabnee <a22311046@uthermosillo.edu.mx>

--- resources/views/artists/create.blade.php
+++ resources/views/artists/create.blade.php
@@ -78,9 +78,11 @@
                             <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                                 {{ __('Already registered?') }}
                             </a>
-                            <x-primary-button class="ml-4">
-                                {{ __('Register') }}
-                            </x-primary-button>
+                            <button>
+                                <span class="ml-4 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" type = "submit">
+                                    Register
+                                </span>
+                            </button>
                         </div>
                 </form>        
             </div> 
