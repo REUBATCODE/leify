bcadaa7f89572eb9e3258903aa0e22ff0ab26137
AngelDabnee <a22311046@uthermosillo.edu.mx>
2024-03-19 19:09:16 -0700

    fix()solucionamos el bug del boton mal posicionado
    Signed-off-by: AngelDabnee <a22311046@uthermosillo.edu.mx>

--- resources/views/artists/index.blade.php
+++ resources/views/artists/index.blade.php
@@ -54,7 +54,6 @@
                             <td>{{$artist->bio}}</td>
                             <td>
                                 <a class="text-white bg-yellow-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-yellow-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800" href="{{ route('artists.view', $artist) }}">Ver</a>
-                                <a class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-red-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800" href="{{ route('artists.view', $artist) }}">Eliminar</a>
                             </td>
                         </tr>
                         @endforeach
